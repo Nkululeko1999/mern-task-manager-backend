d16593ecc9160127c29c574849d7fb719f1ab7a4
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2023-10-19 21:25:42 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,10 @@ import taskRoutes from "./routes/tasks.routes.js";
 import cors from "cors";
 
 const app = express();
-const port =  process.env.PORT || 10000;
+const port =  process.env.PORT || 5000;
 
 app.use(cors({
-   origin: 'https://mern-task-app.onrender.com',
-   methods: 'GET,POST,PUT,DELETE',
-   allowedHeaders: 'Content-Type,Authorization',
-   credentials: true,
+   origin: ["localhost:3000", "https://mern-task-app.onrender.com"
  }));
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);

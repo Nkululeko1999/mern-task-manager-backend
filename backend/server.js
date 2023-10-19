c1c7303ca4e16d511d4124bead6856eedd7d9d6d
Nkululeko1999 <105264276+Nkululeko1999@users.noreply.github.com>
2023-10-19 21:39:40 +0200

    Update cors origin

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port =  process.env.PORT || 5000;
 
 app.use(cors({
-   origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
+   origin: ["http://localhost:3000", "https://mern-task-app-q0sa.onrender.com"],
  }));
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
